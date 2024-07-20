0937525573ef961d0dbedbefe00cea11fe52be7e
dgigur <dgigurda005@mail.ru>
2024-07-20 16:43:29 +0300

    Ввел коментарий для проверки

--- homework2.py
+++ homework2.py
@@ -1,6 +1,6 @@
 number_of_homework = 12
 number_of_hours_spent = 1.5
-course_name = 'Python'
+course_name = 'Python' #Изменение для Git
 time_per_task = number_of_hours_spent/number_of_homework
 print('Курс:', course_name + ",", 'всего задач:', str(number_of_homework) + ",",
       'затрачено часов:', str(number_of_hours_spent) + ",", 'среднее время выполнения',
