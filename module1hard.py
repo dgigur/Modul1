b8c564510454ab84a4449133456561197c599da1
dgigur <dgigurda005@mail.ru>
2024-07-21 11:35:11 +0300

    Update module1hard.py

--- module1hard.py
+++ module1hard.py
@@ -3,7 +3,7 @@ grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5
 students = {'Johny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
 students = sorted(students)
 
-#for i in range(0, len(grades)):
+#for i in range(len(grades)):
 #    dictionary.update({students[i]: round(sum(grades[i])/len(grades[i]), 1)})
 #print(dictionary)
 
